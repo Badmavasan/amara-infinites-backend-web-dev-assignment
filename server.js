e27f4570bb9a69b6f0e8cace30601ffa7652be00
Badmavasan KIROUCHENASSAMY <badmavasan.kirouchenassamy@lip6.fr>
2024-10-21 13:12:27 +0200

    checking

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
-
+const fs = require('fs');
 const path = require('path');
+
+// Resolve the path to the 'models' directory
+const modelsPath = path.resolve(__dirname, 'models');
+
+// Check if 'models' path exists and is a directory
+fs.stat(modelsPath, (err, stats) => {
+    if (err) {
+        console.error(`Error: The path ${modelsPath} does not exist or is inaccessible.`);
+    } else if (stats.isDirectory()) {
+        console.log(`The directory ${modelsPath} exists.`);
+    } else {
+        console.log(`${modelsPath} exists but is not a directory.`);
+    }
+});
+
 const connectDB = require(path.resolve(__dirname, 'config/db'));
 const authRoutes = require(path.resolve(__dirname, 'routes/auth'));
 require('dotenv').config();
